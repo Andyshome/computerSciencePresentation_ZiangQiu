0d32e879a7892da9437fbe359c151059fc2715c6
Andrew Qiu <ziang.qiu@gmail.com>
2018-05-08 11:57:43 -0400

    Initial Commit

---
+++ computerSciencePresentation_ZiangQiu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  computerSciencePresentation_ZiangQiu
+//
+//  Created by Period Three on 2018-05-08.
+//  Copyright © 2018 Period Three. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
